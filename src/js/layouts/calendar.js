ae21f982fa249b6140461123d0314a05c483305c
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-05-15 19:26:23 +0800

    stripped back template

---
+++ src/js/layouts/calendar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//Resources
+import '../../scss/layouts/calendar.scss';
+
+export class Calendar extends Component {
+  render() {
+    return (        
+      <div>
+        <div>
+          <h1>There's a calendar here</h1>
+        </div>
+      </div>
+    );
+  }
+};
